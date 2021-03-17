8bd4a9a6a2b45be5809f24018e894149d475ff0e
tomaspizarro15 <61084368+tomaspizarro15@users.noreply.github.com>
2021-03-17 16:28:10 -0300

    header-done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './App.css';
+import Header from './components/static/header/header';
 
 function App() {
+
+  const [header , setHeader] = useState({ title: "Trillo", links: [{ id: 0, label: "New Trillo" }, { id: 1, label: "my Trillos" }] })
+
+  useEffect(() => {
+    console.log("Render")
+  })
+
   return (
-    <div className="App">
+    <div className="App" onScroll={() => {console.log("Scrolling?")}}>
+      <Header title="my header" links={header.links} />
       
     </div>
   );
--- src/components/static/header/header.tsx
+++ src/components/static/header/header.tsx
@@ -1,21 +1,44 @@
-import React, { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import './header.css'
+
 
 interface HeaderProps {
-    title: String,
-    links: Array<String>,
+    title: string,
+    links: Array<{ id: number, label: string }>,
 }
 
 const Header = (props: HeaderProps) => {
 
+    const [title, setTitle] = useState("")
+    const [scrollPos, setScrollPos] = useState("");
+    const [header , setHeader] = useState(true)
+
     useEffect(() => {
-        
-     }, [])
+        console.log("React DOM Render")
+    } , [header])
+
+
+    const handleScroll = () => {
+        if(!window.scrollY) {
+            setHeader(false)
+        }else {
+            setHeader(true)
+        }
+    }
+
+    window.addEventListener('scroll', handleScroll)
 
 
     return (
         <div className="header_wrapper">
-            <div className="header">
-
+            <div className={header ? "header dis" : "header" }>
+                <ul className="flex_ccr header_links"> {props.links.map((el, i, arr) => {
+                    return (
+                        <li className="header_tools" key={el.id}>
+                            <p>{el.label}</p>
+                        </li>
+                    )
+                })}</ul>
             </div>
         </div>
     )
